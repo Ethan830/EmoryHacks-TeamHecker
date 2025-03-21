b136e22e6df69d9e07f6602612b2fbfc09bf6536
hyunsu0178 <hyunsu0178@gmail.com>
2025-03-21 19:07:30 -0400

    button

--- popup.js
+++ popup.js
@@ -1,21 +1,3 @@
-document.getElementById('apply').addEventListener('click', async () => {
-    const type = document.getElementById('colorType').value;
-    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      args: [type],
-      func: (type) => {
-        const linkId = 'colorblind-adjustment-style';
-        let link = document.getElementById(linkId);
-        if (link) link.remove();
-  
-        link = document.createElement('link');
-        link.id = linkId;
-        link.rel = 'stylesheet';
-        link.href = chrome.runtime.getURL(`styles/${type}.css`);
-        document.head.appendChild(link);
-      }
-    });
-  });
-  
+document.getElementById('testButton').addEventListener('click', () => {
+  chrome.tabs.create({ url: "https://enchroma.com/pages/color-blindness-test" });
+});
