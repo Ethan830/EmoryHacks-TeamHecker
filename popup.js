fcbb112bac1ef41bb27371f2a590d070cb2e97a7
hyunsu0178 <hyunsu0178@gmail.com>
2025-03-21 18:38:07 -0400

    basic setting

--- popup.js
+++ popup.js
@@ -1,11 +1,21 @@
-document.getElementById('changeColor').addEventListener('click', async () => {
+document.getElementById('apply').addEventListener('click', async () => {
+    const type = document.getElementById('colorType').value;
     const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
   
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
-      function: () => {
-        document.body.style.backgroundColor = "lightblue";
-      },
+      args: [type],
+      func: (type) => {
+        const linkId = 'colorblind-adjustment-style';
+        let link = document.getElementById(linkId);
+        if (link) link.remove();
+  
+        link = document.createElement('link');
+        link.id = linkId;
+        link.rel = 'stylesheet';
+        link.href = chrome.runtime.getURL(`styles/${type}.css`);
+        document.head.appendChild(link);
+      }
     });
   });
   
