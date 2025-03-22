47737335d9c018e0b4eb2952f00c01a7f909a371
Hector Liao <nimasile@Hectors-MacBook-Air-6.local>
2025-03-22 17:29:25 -0400

    Resolved merge conflict and cleaned ColorBlind component

--- src/routes/ColorBlind/index.js
+++ src/routes/ColorBlind/index.js
@@ -3,22 +3,6 @@ import Option from "../../components/Option";
 import BackButton from "../../components/BackButton";
 import styles from "./index.css";
 
-<<<<<<< HEAD
-  const ColorBlind = () => {
-    const [selectedMode, setSelectedMode] = useState("protanopia");
-  
-    const handleApply = () => {
-      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        chrome.tabs.sendMessage(tabs[0].id, {
-          action: "applyColorFilter",
-          mode: selectedMode,
-        });
-      });
-    };
-  const handleClick = () => {
-    window.open("https://enchroma.com/pages/color-blindness-test");
-
-=======
 const ColorBlind = () => {
   const [selectedMode, setSelectedMode] = useState("protanopia");
 
@@ -30,9 +14,9 @@ const ColorBlind = () => {
       });
     });
   };
+
   const handleClick = () => {
     window.open("https://enchroma.com/pages/color-blindness-test");
->>>>>>> fe08fb7aaaea663399a422ee691d4c0d5abdcb38
   };
 
   return (
@@ -41,38 +25,12 @@ const ColorBlind = () => {
 
       <h1>Choose Your Type</h1>
 
-<<<<<<< HEAD
       <Option label="Protanopia" color="red" onClick={() => setSelectedMode("protanopia")} />
       <Option label="Deuteranopia" color="green" onClick={() => setSelectedMode("deuteranopia")} />
       <Option label="Tritanopia" color="blue" onClick={() => setSelectedMode("tritanopia")} />
 
       <button className="applyBtn" onClick={handleApply}>Apply</button>
-=======
-      <Option
-        label="Protanopia"
-        color="red"
-        onClick={() => setSelectedMode("protanopia")}
-      />
-      <Option
-        label="Deuteranopia"
-        color="green"
-        onClick={() => setSelectedMode("deuteranopia")}
-      />
-      <Option
-        label="Tritanopia"
-        color="blue"
-        onClick={() => setSelectedMode("tritanopia")}
-      />
-
-      <button className="applyBtn" onClick={handleApply}>
-        Apply
-      </button>
->>>>>>> fe08fb7aaaea663399a422ee691d4c0d5abdcb38
-      <button id="testButton" onClick={handleClick}>
-        Take Color Blind Test
-      </button>
-
-      <script src="popup.js"></script>
+      <button id="testButton" onClick={handleClick}>Take Color Blind Test</button>
     </div>
   );
 };
