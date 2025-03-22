6f56cb98e717647755ccfa2aafbc80c3b38b24ca
hyunsu0178 <hyunsu0178@gmail.com>
2025-03-21 20:56:32 -0400

    button

--- popup.js
+++ popup.js
@@ -2,3 +2,12 @@
 document.getElementById("testButton").onclick = function () {
     window.open("https://enchroma.com/pages/color-blindness-test", "_blank");
 };
+document.querySelectorAll('.colorblind-checkbox').forEach((checkbox) => {
+    checkbox.addEventListener('change', function() {
+        if (this.checked) {
+            document.querySelectorAll('.colorblind-checkbox').forEach((cb) => {
+                if (cb !== this) cb.checked = false; // Uncheck others
+            });
+        }
+    });
+});
