3caf0a23a6a4b58d967bff86429c0361bcc01e86
Ethan <ethan.yang@25sprout.com>
2025-03-23 00:01:07 -0400

    app logo changes

--- src/popup/App/App.js
+++ src/popup/App/App.js
@@ -5,10 +5,19 @@ import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { ColorBlind, Font } from "../../routes";
 import fontIcon from "../../images/font.png";
 import fonticon from "../../images/color.png";
+import appIcon from "../../images/logo-transparent.png";
 
 const Home = () => {
   return (
     <div style={{ padding: "1rem", width: "220px" }}>
+      <img
+        src={appIcon}
+        alt="logo"
+        style={{
+          margin: "24px 40px",
+          height: "50px",
+        }}
+      />
       <ToggleSetting label="Font" route="/font" icon={fontIcon} />
       <ToggleSetting label="Colorblind" route="/colorblind" icon={fonticon} />
     </div>
