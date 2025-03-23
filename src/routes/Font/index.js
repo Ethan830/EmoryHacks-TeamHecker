15c41bc585f3e7c911aca52f2b0a63460f8654be
hyunsu0178 <hyunsu0178@gmail.com>
2025-03-22 20:45:23 -0400

    range bar animation

--- src/routes/Font/index.js
+++ src/routes/Font/index.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import BackButton from "../../components/BackButton";
 import Switch from "../../components/Switch";
 import './index.css'
 
 const Font = () => {
+  const [fontSize, setFontSize] = useState(16);
   const handleSlide = () => {
     // use useEffect, edit later
     const slider = document.getElementById("fontSlider");
@@ -19,7 +20,14 @@ const Font = () => {
       <Switch label={"Font"} storageKey="fontToggle" />
       <div className="font-size-container" onClick={handleSlide}>
         <label for="fontSlider">Font Size</label>
-        <input type="range" id="fontSlider" min="10" max="40" value="16" />
+        <input
+          type="range"
+          id="fontSlider"
+          min="10"
+          max="40"
+          value={fontSize}
+          onChange={(e) => setFontSize(Number(e.target.value))}
+        />
         <div className="preview-text" id="previewText">
           This is a preview text.
         </div>
